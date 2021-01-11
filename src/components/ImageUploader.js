c8e026d6a558754f680536ee26f774d76045bffc
Petar Obradovic <ns.o.petar@gmail.com>
2021-01-11 19:09:22 +0100

    udpate

--- src/components/ImageUploader.js
+++ src/components/ImageUploader.js
@@ -1,8 +1,10 @@
 import {useRef, useState} from 'react';
+import jpeg from 'jpeg-js';
+
 
 const ImageUploader = () =>  {
     const [file, setFile] = useState(null);
-    const [file_encrypted] = useState(null);
+    const [file_encrypted,setFileEncrypted] = useState(null);
     const file_ref = useRef();
 
 
@@ -11,34 +13,66 @@ const ImageUploader = () =>  {
             url: URL.createObjectURL(event.target.files[0]),
             data: event.target.files[0]
         };
-        
+
         setFile(data);
 
     }
 
     const imageProcess = () => {
         const cv = document.querySelector("#cv");
+        const cv2 = document.querySelector("#cv");
         const img1 = document.querySelector("#image");
     
         let c = cv.getContext("2d");
+        let c2 = cv2.getContext("2d");
         
         let img = new Image()
         img.src = window.URL.createObjectURL(file.data);
         img.onload = () => {
-            c.drawImage(img1, 0,0,img.width, img.height);
-            var imgData = c.getImageData(0, 0, img.width, img.height);
-
+            const w = img.width;
+            const h = img.height;
+            c.canvas.width = w;
+            c.canvas.height = h;
+            c.drawImage(img1, 0,0,w, h);
+            // c2.createImageData(img.width, img.height);
+            var imgData = c.getImageData(0, 0, w, h);
+            var frameData = new Buffer(w * h * 4);
             // get color pixels rgba
             var pixels = []
             for (var i = 0; i < imgData.data.length; i += 4) {
                 const pixel = {
-                    r: imgData.data[i],
-                    g: imgData.data[i+1]+10,
-                    b: imgData.data[i+2],
+                    r: imgData.data[i] + Math.floor(Math.random() * 100) - 50,
+                    g: imgData.data[i+1] + Math.floor(Math.random() * 100) - 50 ,
+                    b: imgData.data[i+2] + Math.floor(Math.random() * 100) - 50,
                     a: imgData.data[i+3]
                 };
                 pixels.push(pixel);
             }
+
+
+           for(const i in pixels){
+               frameData[i*4 ]     = pixels[i].r;
+               frameData[i*4 + 1] = pixels[i].g;
+               frameData[i*4 + 2] = pixels[i].b;
+               frameData[i*4 + 3] = pixels[i].a;
+           }
+
+           var rawImageData = {
+            data: frameData,
+            width: w,
+            height:h,
+          };
+          console.log(file.data);
+
+          var jpegImageData = jpeg.encode(rawImageData);
+          const src  = URL.createObjectURL(
+            new Blob([jpegImageData.data], { type: file.data.type } /* (1) */)
+          );
+          console.log(src);
+          setFileEncrypted(src)
+
+
+
             console.log(pixels);
         }
     }
@@ -58,9 +92,9 @@ const ImageUploader = () =>  {
         <div style={{flex: 1}}>
             { file && (
                 <div style={{flexDirection: 'row'}}>
-                    <div style={{alignItems:'unset'}}> 
+                    <div > 
                         <img id="image" className="App-logo" src={file?.url}  alt="" />
-                        <img id="image2" className="App-logo" src={file_encrypted}  alt="" />
+                        <img id="image" className="App-logo" src={file_encrypted}  alt="" />
                     </div>
                     
                     <div style={{flexDirection: 'column', justifyContent:'space-between', flex: 1}}>
@@ -81,6 +115,7 @@ const ImageUploader = () =>  {
                 <div> 
                     <input ref={file_ref} type="file" onChange={handleChange} style={{display: 'none'}}/>
                     <canvas id="cv" style={{display: 'none'}} />
+                    <canvas id="cv2" style={{display: 'none'}} />
                 </div>
             {/*  */}
         </div>
